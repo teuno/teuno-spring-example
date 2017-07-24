6e422397faf8a44c58bc22d026215811e151a4be
teuno <Teuno.Hooijer@topicus.nl>
2017-07-24 11:03:20 +0200

    Initial commit

---
+++ src/main/java/com/teuno/springexample/TeunoSpringExampleApplication.java
@@ -0,0 +1,12 @@
+package com.teuno.springexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeunoSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeunoSpringExampleApplication.class, args);
+	}
+}
